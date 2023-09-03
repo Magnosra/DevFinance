8ada8a08e40548f42c404cb78dbe570f7ef9292f
Magno Rodrigues <magnoestudante@hotmail.com>
2023-09-03 17:03:18 -0300

    Execução Dev Finance

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,6 @@
 // cypress/support/e2e.js
-  
+import '@shelex/cypress-allure-plugin';
+require('@shelex/cypress-allure-plugin');
   // Configuração global (exemplo: URL base)
   Cypress.config('baseUrl', 'https://dev-finances.vercel.app/');
   
