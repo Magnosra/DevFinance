7111badfe0dbbdf7ef6a3ee1a463d8dbd99bf81b
Magno Rodrigues <magnoestudante@hotmail.com>
2023-09-03 13:16:32 -0300

    Execução Dev Finance

--- cypress/e2e/DevFinance/desconto.cy.js
+++ cypress/e2e/DevFinance/desconto.cy.js
@@ -6,7 +6,7 @@ describe('Teste Saída', () => {
         cy.visit('/')
         cy.get('.float-button').click()
         cy.get('#description').type('Compra Supermercado')
-        cy.get('#amount').type('')
+        cy.get('#amount').type('-5')
         cy.get('#date').type('2023-09-02')
         cy.get('button').contains('Salvar').click()
         cy.get('tr > .description').contains('Compra Supermercado')
