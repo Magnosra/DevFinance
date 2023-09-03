291bb3549edd9683c43c55f4f6e293da92190c8a
Magno Rodrigues <magnoestudante@hotmail.com>
2023-09-03 16:29:07 -0300

    Execução Dev Finance

---
+++ cypress/e2e/DevFinance/remocao.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress"/>
+describe('Teste Saída', () => {
+    
+    it('Validacao Desconto', () => {
+        
+        cy.visit('/')
+        cy.get('.float-button').click()
+        cy.get('#description').type('Compra Supermercado')
+        cy.get('#amount').type('10')
+        cy.get('#date').type('2023-09-02')
+        cy.get('button').contains('Salvar').click()
+        cy.get('tr > .description').contains('Compra Supermercado')
+        cy.get('.float-button').click()
+        cy.get('#description').type('Desconto Compra')
+        cy.get('#amount').type('-5')
+        cy.get('#date').type('2023-09-02')
+        cy.get('button').contains('Salvar').click()
+        cy.get('tr > .description').contains('Desconto Compra')
+        cy.get('[data-index="1"] > :nth-child(4) > .remove').click()
+        
+        
+    })
+});
