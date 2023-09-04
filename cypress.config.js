8b431245821ef090ffc980ec3be49f8be367edc3
Magno Rodrigues <magnoestudante@hotmail.com>
2023-09-03 22:32:20 -0300

    Execucao Testes Dev

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,13 @@
 const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 // import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            on('file:preprocessor', webpackPreprocessor);
-            allureWriter(on, config);
-            return config;
-        },
-        env: {
-            allureReuseAfterSpec: true
+          allureWriter(on, config);
+          return config;
         }
     }
 });
-      
-    },
-    
-    
-    
-  },
 
-  
-  
-});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,6 +1,16 @@
 // cypress/support/e2e.js
-import '@shelex/cypress-allure-plugin';
-require('@shelex/cypress-allure-plugin');
+
   // Configuração global (exemplo: URL base)
   Cypress.config('baseUrl', 'https://dev-finances.vercel.app/');
   
+  //import '@shelex/cypress-allure-plugin';
+
+  require('@shelex/cypress-allure-plugin');
+
+  //npx cypress run --config video=false --env allure=true 
+
+  import '@shelex/cypress-allure-plugin';
+
+Cypress.Commands.add('allureStep', (name, stepFunction) => {
+  cy.allure().step(name, stepFunction);
+});
