307e67bdcc1b61ac101881dfe18df915034672fb
Magno Rodrigues <magnoestudante@hotmail.com>
2023-09-05 20:55:55 -0300

    Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+//import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
-module.exports = (on, config) => {
-    allureWriter(on, config);
-    return config;
-};
+module.exports = defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+
+    }
+
+
+});
+
